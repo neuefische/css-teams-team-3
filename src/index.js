5f09eb453d57224f25816b72a7407fa65d8e02eb
leonp5 <leonpe@web.de>
2019-11-08 16:53:04 +0100

    try add detailsite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// import { create } from "domain";
+
 const vegetables = [
   {
     name: "Pumpkin",
@@ -49,3 +51,29 @@ vegetables.forEach(function(result) {
 
   container.innerHTML += content;
 });
+
+function createDetail(result) {
+  const content = `
+  <div class="card-element">
+    <div class="card-element__headline">
+      <h2>${result.name}</h2>
+    </div>
+    <div class="card-element__main">
+      <div class="card-element__img">
+        <img
+          src="${result.pic}"
+          alt="${result.name}"
+        />
+      </div>
+      <div class="card-element__content">
+        <p>
+          ${result.description}
+        </p>
+        <button><a href="/details.html">details</a></button>
+      </div>
+    </div>
+  </div>
+  `;
+
+  console.log(content);
+}
